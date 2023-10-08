9da028cbc72fe2dcc19ba59c830299c73c94036e
subhrambiswas <subhrambiswas2014@gmail.com>
2023-10-08 08:50:24 +0530

    changes

--- src/main/java/com/example/crudspringboot/BookService.java
+++ src/main/java/com/example/crudspringboot/BookService.java
@@ -2,21 +2,76 @@ package com.example.crudspringboot;
 
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+
 //defining the business logic
 @Service
 public class BookService
 {
     BooksRepository booksRepository;
-    //getting all books record by using the method findaAll() of CrudRepository
+    //getting all books record by using the method findAll() of CrudRepository
+
+    private static final List<Books> books = new ArrayList<>();
+    static {
+        books.add(new Books(1, "Mid Summer Night Dream",  "Adam",1000));
+        books.add(new Books(2,"As You Like It","EVE",2000));
+        books.add(new Books(3,"GoodFood","Tolstoy",300));
+        books.add(new Books(4, "Fundamental C",  "Adam",10000));
+        books.add(new Books(5," Java Fundamental","EVE",2500));
+        books.add(new Books(6,"Good code","Tolstoy",3000));
+
+    }
     public List<Books> getAllBooks()
     {
+/*
         List<Books> books = new ArrayList<Books>();
-        booksRepository.findAll().forEach(books1 -> books.add(books1));
+        booksRepository.findAll().forEach(books::add);
+*/
         return books;
     }
-    //getting a specific record by using the method findById() of CrudRepository
+
+
+    public List<Integer> getBooksAbove1000() {
+
+        //return books.stream().filter(i -> i.getPrice() >= 1000).collect(Collectors.toList());
+
+
+        //Find the length of all book name
+        return books.stream().map(i -> i.getBookName().length()).collect(Collectors.toList());
+
+    }
+
+    public int getBooksPriceAggregations() {
+
+        //Sum of books price
+//        Integer lengthwise = books.stream().collect(Collectors.summingInt(i -> i.getPrice()));
+//        return lengthwise;
+
+      return books.stream().mapToInt(Books::getPrice).sum();
+
+
+
+    }
+
+    public List<Books>  getBooksBasedOnCondition() {
+
+        //Find second-highest price after sort in descending order
+//           List<Books> bookPrice = books.stream()
+//                                        .sorted(Comparator.comparingInt(Books::getPrice).reversed())
+//                                        .collect(Collectors.toList());
+//           return bookPrice.get(0);
+
+
+        //Find the first element from the list
+//          Optional<Books> bookPrice = books.stream().findFirst();
+//          return bookPrice.get();
+
+        //Fetching books on selective conditions on price and author
+        return books.stream().filter(i -> i.getAuthor().equalsIgnoreCase("Adam")).collect(Collectors.toList());
+
+    }
+
     public Books getBooksById(int id)
     {
         return booksRepository.findById(id).get();
@@ -25,9 +80,14 @@ public class BookService
 
 
     //saving a specific record by using the method save() of CrudRepository
-    public void saveOrUpdate(Books books)
+    public void saveOrUpdate(Books book)
     {
-        booksRepository.save(books);
+        //Using repository and JPA integration
+//        booksRepository.save(books);
+
+        books.add(book);
+
+
     }
 
 
@@ -38,11 +98,9 @@ public class BookService
     }
 
     //updating a record
-    public void update(Books books, int bookid)
+    public void update(Books books)
     {
         booksRepository.save(books);
     }
 
-
-    //subhram test LTIMINDTREE
 }
--- src/main/java/com/example/crudspringboot/Books.java
+++ src/main/java/com/example/crudspringboot/Books.java
@@ -1,15 +1,17 @@
 package com.example.crudspringboot;
 
 
+import lombok.Getter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@Getter
 @Entity
 @Table
-public class Books
-{
+public class Books {
     //Defining book id as primary key
     @Id
     @Column
@@ -21,39 +23,33 @@ public class Books
     @Column
     private int price;
 
+    public Books(int bookId, String bookname, String author, int price) {
 
-    public int getBookId()
-    {
-        return bookId;
+        this.bookName = bookname;
+        this.bookId = bookId;
+        this.author = author;
+        this.price = price;
     }
+
 
     public void setBookId(int bookId) {
         this.bookId = bookId;
-    }
-
-    public String getBookName() {
-        return bookName;
     }
 
     public void setBookName(String bookName) {
         this.bookName = bookName;
     }
 
-    public String getAuthor() {
-        return author;
-    }
-
     public void setAuthor(String author) {
         this.author = author;
-    }
-
-    public int getPrice() {
-        return price;
     }
 
     public void setPrice(int price) {
         this.price = price;
     }
 
-
+    @Override
+    public String toString() {
+        return "Books [bookId=" + bookId + ", " + "bookName=" + bookName + ", " + "author=" + author + ", " + "price=" + price + "]";
+    }
 }
